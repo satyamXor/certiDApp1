d4e559defe0568492e5419fb8aea19c872fd3552
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2020-10-23 03:33:09 +0530

    Add  Dynamic route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { RegisterCertificate } from './components/RegisterCertificate/RegisterCertificate';
 import { ViewCertificate } from './components/ViewCertificate/ViewCertificate';
+import { ViewCertificate1 } from './components/ViewCertificate1/ViewCertificate1';
 import { ListAuthorities } from './components/ListAuthorities/ListAuthorities';
 import { SignCertificate } from './components/SignCertificate/SignCertificate';
 import './App.css';
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/RegisterCertificate" exact component={RegisterCertificate} />
           <Route path="/ViewCertificate" exact component={ViewCertificate} />
+          <Route path="/ViewCertificate/:hash" exact component={ViewCertificate1} />
           <Route path="/ListAuthorities" exact component={ListAuthorities} />
           <Route path="/SignCertificate" exact component={SignCertificate} />
         </Switch>
