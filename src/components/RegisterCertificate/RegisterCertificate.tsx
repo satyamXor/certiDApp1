eda3548aaee3879133b7dc77f808055ef28d6986
pranjal1127 <2018kucp1127@iiitkota.ac.in>
2020-10-23 07:39:26 +0530

    page /RegisterCertifiate

--- src/components/RegisterCertificate/RegisterCertificate.tsx
+++ src/components/RegisterCertificate/RegisterCertificate.tsx
@@ -1,15 +1,65 @@
 import React, { Component } from 'react';
 import './RegisterCertificate.css';
-// import { Link } from 'react-router-dom';
-import {  Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import {  Alert,Form } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import ethers from 'ethers'
+import { CertificateBox } from '../CertificateBox';
+// import { assert } from 'console';
 
 type State = {
   bunchModal: boolean;
+  signHash : string;
+  done : boolean;
+  hash : string;
+  byte : string; 
+  certi : boolean;
+  Args : string [];
+};
+
+const  TxnDetails = ({ hash, byte }) => {
+  return (
+    <>
+      <br />
+      <i className="fa fa-check-circle" style={{ fontSize: '8em', color: '#54BE3D' }}></i>
+      <p>
+        {' '}
+        Transaction Sussessful <br /> Your Certificate has been Registered {' '}
+      </p> 
+      <br /> <br /> <br />
+      <table className="table">
+        <tr>
+          <td> Transaction Hash : </td>
+          <td>
+            <a href={'https://eraswap.info/txn/' + hash}>{hash} </a>{' '}
+          </td>
+        </tr>
+        <tr>
+          <td>TimeStamp</td>
+          <td>{Date.now()}</td>
+        </tr>
+        <tr>
+          <td>Your Certificate</td>
+          <td>
+            <Link to={'/Survey/Surveys/' + byte}>{byte}</Link>
+          </td>
+        </tr>
+        <hr />
+      </table>
+    </>
+  );
 };
 
 export class RegisterCertificate extends Component<State> {
   state: State = {
     bunchModal: false,
+    signHash : '',
+    done : false,
+    hash : '',
+    byte : '',
+    certi: false,
+    Args : []
+
   };
 
   handleClose = () => {
@@ -18,6 +68,58 @@ export class RegisterCertificate extends Component<State> {
     });
   };
 
+  handleRegister = async (e) => {
+    e.preventDefault();
+    const Args = this.state.signHash.split(","); 
+    console.log(Args);
+    this.setState({Args : Args});
+    const color:string[] = ["danger","primary","success"];
+
+    if(window.wallet){
+      try {
+		//   assert(Args.length === 3)
+      const sur = await window.certificateInstance.connect(window.wallet)
+                    .registerCertificates(Args[0],Args[1],Args[2]);
+        const receipt = await sur.wait();
+        console.log('TXN Hash :', receipt);
+        Swal.fire({
+          icon: 'success', 
+          title: 'Great 👍',
+		  text: 'You have Register you Certificate ', 
+        });  
+        this.setState({
+          done: true,
+          hash: receipt.transactionHash,
+          byte: receipt.events[0].args[0]
+        }); 
+ 
+
+      } catch (e) {
+        const add = await window.wallet.getAddress();
+        const x = new ethers.VoidSigner(add, window.providerESN);
+        try {
+          const A = await window.certificateInstance.connect(x).estimateGas.registerCertificates(Args[0],Args[1],Args[2]);
+          console.log(A);
+        } catch (e) {
+          console.log('Error is : ', e);
+        
+        Swal.fire({
+          icon: 'error', 
+          title: 'Oops...', 
+          text: `${e}`,
+        });
+        console.log(e); 
+      } }
+      } 
+
+    else Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Please Connect to wallet!',
+    });
+    
+    
+  }
   render() {
     return (
       <div className="homebg">
@@ -46,20 +148,27 @@ export class RegisterCertificate extends Component<State> {
                </div>
 
                <h4 className="mt80 font-weight-bold mb20">Paste your signed certificate in the below box:</h4>  
-               <textarea  className="card-round col-md-12" ></textarea>
-               <Alert variant="danger"   className="mt40">
+               <Form >
+               <textarea  className="card-round col-md-12" 
+                name='signHash' 
+                value={this.state.signHash}  
+                onChange={e=>this.setState({signHash:e.target.value})} ></textarea>
+               <Alert variant="danger"  className="mt40">
                   <p>If you want to register your certificate, you will
                      need Metamask installed. If you are here only to view certificates,
                       installing Metamask is not necessary</p>
                   
                 </Alert>
-                <div className="">
-                     <a href="/"  className="btn btn-primary btn-xl js-scroll-trigger combtn combtn1 mt20 mb30">
+              </Form>
+                <button onClick={()=>{this.setState({certi:true});const Args = this.state.signHash.split(","); this.setState({Args : Args});}} className="btn btn-primary ">
+                     preview
+                </button>
+                <button onClick={this.handleRegister} className="btn btn-primary btn-xl js-scroll-trigger combtn combtn1 mt20 mb30">
                      Register Certificate
-                       </a>
-                </div>
-
+                </button>
             </div>
+            {this.state.certi ? <CertificateBox Hash={this.state.Args[0]} address={this.state.Args[2]}/> : null }
+            {this.state.done ?(<TxnDetails  hash={this.state.hash} byte={this.state.byte} />) : null}
           </div>
         </div>
 
