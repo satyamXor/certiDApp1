6d663c38488366418590c089dc7b799c2258d95a
Pranjal Agrawal <2018kucp1127@iiitkota.ac.in>
2020-10-30 15:47:06 +0530

    Update ipfs.js

--- src/components/ipfs.js
+++ src/components/ipfs.js
@@ -1,6 +1,8 @@
 import * as IpfsHttpClient from 'ipfs-http-client';
 
-const ipfs = IpfsHttpClient({ host: 'ipfs.eraswap.cloud', port: '5005', protocol: 'http' });
+
+const ipfs = IpfsHttpClient({ host: 'ipfs.eraswap.cloud', port: '5005', protocol: 'https' });
+// const ipfs = IpfsHttpClient({ host: 'ipfs.eraswap.cloud', port: '5005', protocol: 'http' });
 // const ipfs = IpfsHttpClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' });
 
 export default ipfs;
